5dbea66e04e8c4fb014405a660ace056c9d87ee9
aaronwb <zmtzwb@gmail.com>
2020-07-17 17:19:32 +0800

    optimise popup menu

--- lib/main.dart
+++ lib/main.dart
@@ -5,66 +5,85 @@ void main() {
   runApp(App());
 }
 
-class App extends StatelessWidget{
+class App extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text("wechat"),
-          actions: <Widget>[
-            IconButton(
-              icon: Icon(Icons.search),
-              onPressed: (){},
-            ),
-            PopupMenuButton(
-              icon: Icon(Icons.add_circle_outline),
-              itemBuilder: (BuildContext context) {
-                return <PopupMenuEntry<String>>[
-                  PopupMenuItem(
-                    child: Row(
-                      children: <Widget>[
-                        Icon(Icons.message, color: Colors.black26,),
-                        Text("发起群聊")
-                      ],
-                    ),
+        home: Scaffold(
+      appBar: PreferredSize(
+        preferredSize: Size.fromHeight(40),
+        child: AppBar(title: Text("wechat"), actions: <Widget>[
+          IconButton(
+            icon: Icon(Icons.search),
+            onPressed: () {
+              print("ok");
+            },
+          ),
+          PopupMenuButton(
+            icon: Icon(Icons.add_circle_outline),
+            offset: Offset(0, 50),
+            shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.all(Radius.circular(8))),
+            itemBuilder: (BuildContext context) {
+              return <PopupMenuEntry<String>>[
+                PopupMenuItem(
+                  height: 40,
+                  child: Row(
+                    children: <Widget>[
+                      Icon(
+                        Icons.message,
+                        color: Colors.black26,
+                      ),
+                      Text("发起群聊")
+                    ],
                   ),
-                  PopupMenuItem(
-                    child: Row(
-                      children: <Widget>[
-                        Icon(Icons.supervised_user_circle, color: Colors.black26,),
-                        Text("添加朋友")
-                      ],
-                    ),
+                ),
+                new PopupMenuDivider(),
+                PopupMenuItem(
+                  height: 40,
+                  child: Row(
+                    children: <Widget>[
+                      Icon(
+                        Icons.supervised_user_circle,
+                        color: Colors.black26,
+                      ),
+                      Text("添加朋友")
+                    ],
                   ),
-                  PopupMenuItem(
-                    child: Row(
-                      children: <Widget>[
-                        Icon(Icons.find_in_page, color: Colors.black26,),
-                        Text("扫一扫")
-                      ],
-                    ),
-                  )
-                ];
-              },
-            ),
-
-          ],
-        ),
-        body: IndexPage(),
-      )
-    );
+                ),
+                new PopupMenuDivider(),
+                PopupMenuItem(
+                  height: 40,
+                  child: Row(
+                    children: <Widget>[
+                      Icon(
+                        Icons.find_in_page,
+                        color: Colors.black26,
+                      ),
+                      Text("扫一扫")
+                    ],
+                  ),
+                )
+              ];
+            },
+          ),
+        ]),
+      ),
+      body: IndexPage(),
+    ));
   }
 }
 
-class IndexPage extends StatelessWidget{
+class IndexPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Row(
         children: <Widget>[
-          FlatButton(onPressed: () {  },
-          child: Text("ok"),)
+          FlatButton(
+            onPressed: () {},
+            child: Text("ok"),
+          )
         ],
       ),
     );
